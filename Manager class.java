519a4f652e162cf7be0f1564464e1464263a697b
anish2626 <se22uari018@mahindrauniversity.edu.in>
2024-11-04 18:59:00 +0530

    Update Manager class.java

--- Manager class.java
+++ Manager class.java
@@ -1,4 +1,3 @@
-
 public class Manager extends Employee {
 
     public Manager(String employeeId, String employeeName, Bank bank, Branch branch, 
@@ -7,34 +6,37 @@ public class Manager extends Employee {
         super(employeeId, employeeName, bank, branch, designation, department, salary, phoneNumber, email, joiningDate);
     }
 
-    
     public void approveLoan(LoanApplication application) {
         if (application != null && !application.isApproved()) {
-            application.setApproved(true);
-            System.out.println("Loan application for " + application.getApplicantName() + " approved.");
+            application.updateLoanStatus("Approved");
+            application.setVerifiedBy(this);
+            System.out.println("Loan application for " + application.getApplicantName() + " approved by " + getEmployeeName());
         } else {
             System.out.println("Loan application is already approved or null.");
         }
     }
 
-
     public void generateEmployeeReport(Employee employee) {
         if (employee != null) {
-            System.out.println("Generating report for Employee ID: " + employee.getEmployeeID());
+            System.out.println("Employee Report for ID: " + employee.getEmployeeId());
             System.out.println("Name: " + employee.getEmployeeName());
             System.out.println("Designation: " + employee.getDesignation());
+            System.out.println("Department: " + employee.getDepartment());
             System.out.println("Salary: " + employee.getSalary());
         } else {
             System.out.println("Invalid employee information.");
         }
     }
 
+    public double getSalary() {
+        return super.getSalary();
+    }
 
     public void provideBonus(Employee employee, double bonus) {
         if (employee != null && bonus > 0) {
             double updatedSalary = employee.getSalary() + bonus;
-            employee.setSalary(updatedSalary);
-            System.out.println("Bonus of " + bonus + " provided to Employee ID: " + employee.getEmployeeID());
+            employee.updateSalary(updatedSalary);
+            System.out.println("Bonus of " + bonus + " provided to Employee ID: " + employee.getEmployeeId());
         } else {
             System.out.println("Invalid employee or bonus amount.");
         }
